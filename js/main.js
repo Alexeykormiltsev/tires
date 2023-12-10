490c1878729eed9efa4d6dcdf06c1b2211b966fb
Alexeykormiltsev <cormiltsev@yandex.ru>
2023-12-10 14:56:47 +0500

    added first__slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-$(function(){
-
+const swiper = new Swiper(".first__slider", {
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 7000,
+  },
+  speed: 1200,
+  spaceBetween: 120,
+  pagination: true,
+  navigation: false,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
