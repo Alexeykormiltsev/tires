7be110a8db156a03ab4cda005e705e3e50b45b04
Alexeykormiltsev <cormiltsev@yandex.ru>
2024-01-04 19:29:23 +0500

    new13/2

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ const swiper = new Swiper(".first__slider", {
     clickable: true,
   },
 });
+const swip = new Swiper(".maker__slider", {
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 4000,
+  },
+  speed: 1200,
+  spaceBetween: 35,
+  pagination: true,
+  navigation: false,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
